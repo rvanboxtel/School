ba29c919eb0c55aef5c5eb5a9ce474c60ac0bb2f
rvanboxtel <rvanboxtel@roc-dev.com>
2016-12-08 10:42:49 +0100

    Hoi

---
+++ Javascript/BoterKaasEnEieren/js/GameMechanics.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Romek/Romy  on 01-12-16.
+ * Boter kaas en eieren mechanics
+ */
+
+var cel1 = document.getElementById("cell1x1");
+cel1.onclick = function changeImage() {
+    var image = document.getElementById('cell1x1');
+    image.src = "img/cross.jpg";
+}
+var cel2 = document.getElementById("cell1x2");
+cel2.onclick = function changeImage() {
+    var image = document.getElementById('cell1x2');
+    image.src = "img/cross.jpg";
+}
+var cel3 = document.getElementById("cell1x3");
+cel3.onclick = function changeImage() {
+    var image = document.getElementById('cell1x3');
+    image.src = "img/cross.jpg";
+}
+var cel4 = document.getElementById("cell2x1");
+cel4.onclick = function changeImage() {
+    var image = document.getElementById('cell2x1');
+    image.src = "img/cross.jpg";
+}
+var cel5 = document.getElementById("cell2x2");
+cel5.onclick = function changeImage() {
+    var image = document.getElementById('cell2x2');
+    image.src = "img/cross.jpg";
+}
+var cel6 = document.getElementById("cell2x3");
+cel6.onclick = function changeImage() {
+    var image = document.getElementById('cell2x3');
+    image.src = "img/cross.jpg";
+}
+var cel7 = document.getElementById("cell3x1");
+cel7.onclick = function changeImage() {
+    var image = document.getElementById('cell3x1');
+    image.src = "img/cross.jpg";
+}
+var cel8 = document.getElementById("cell3x2");
+cel8.onclick = function changeImage() {
+    var image = document.getElementById('cell3x2');
+    image.src = "img/cross.jpg";
+}
+var cel9 = document.getElementById("cell3x3");
+cel9.onclick = function changeImage() {
+    var image = document.getElementById('cell3x3');
+    image.src = "img/cross.jpg";
+}
---
+++ Javascript/BoterKaasEnEieren/js/bke.js
@@ -0,0 +1,15 @@
+/***************************************************
+ * bke.js
+ * -------------------------------------------------
+ * In dit bestand staat alle javascript code
+ * om ons spel te laten werken.
+ *
+ **************************************************/
+
+
+
+btnStartStop = document.querySelector('Button');
+
+console.log("This is an Element of the type: ", btnStartStop.nodeType);
+console.log("The innerHTML is: " + btnStartStop.innerHTML);
+console.log("The childnode is: " + btnStartStop.childNodes.length);
